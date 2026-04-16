80ece35784b14d9e906f6b5749ba14c20f8f9770
aprhyvs <173691141+aprhyvs@users.noreply.github.com>
2026-04-16 22:23:54 +0800

    update initial gameboard

--- script.js
+++ script.js
@@ -7,36 +7,42 @@ function createPlayer (name) {
   return { name, addPoint, getPoint }
 }
 
+const board = (() => {
+  // 1. store the gameboard as an array inside of a Gameboard object
+  const gameBoard = {
+      board: [[1, 2, 3], [4, 5, 6], [7, 8, 9]] 
+  }
+  
+  const displayBoard = () => {
+    for ( let row of gameBoard.board ) {
+      const rowString = row.join(' ');
+      console.log(rowString);
+    } 
+  }
+
+  const changeBoard = (row, column, change) => {
+    //check if cell has been changed
+    const cellToChange = gameBoard.board[row][column]
+    if (typeof cellToChange == 'string') {
+      console.log(`this is already marked. its ${cellToChange}.`)
+      return;
+    }
+
+    gameBoard.board[row][column] = change
+
+    displayBoard()
+  }
+
+  return { displayBoard, changeBoard }
+})();
+
+// 2. your players are stored in objects.
 const players = {
   player1: createPlayer("Joe"),
   player2: createPlayer("Mama")
 }
 
-players.player1.points = 69 // points is a private variable.
-console.log(players.player1)
-console.log(players.player1.getPoint())
-console.log(players.player1.points)
-
+// 3. probably want an object to control the flow of the game.
 const gameFlow = {
 
 }
-
-// 2. store the gameboard as an array inside of a Gameboard object
-/*
-  const gameBoard = {
-    board: [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-  }
-*/
-
-const board = (() => {
-  const gameBoard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-
-  const displayController = () => { 
-    for ( let row of gameBoard ) {
-      const rowString = row.join(' ');
-      console.log(rowString);
-    }
-  }
-
-  return { displayController }
-})();
