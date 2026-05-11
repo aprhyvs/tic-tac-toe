9365dd39de151f281fc3ff7a16bf2f406a8c4394
aprhyvs <173691141+aprhyvs@users.noreply.github.com>
2026-05-11 22:59:35 +0800

    add displayBoard DOM render

--- script.js
+++ script.js
@@ -10,37 +10,34 @@ function createPlayer (name) {
 const board = (() => {
   // 1. store the gameboard as an array inside of a Gameboard object
   const gameBoard = [
-    [null, null, null], 
-    [null, null, null],
-    [null, null, null]
+    // [null, null, null],
+    // [null, null, null],
+    // [null, null, null]
+    ['X','O','X'],
+    ['O','O','X'],
+    ['O','X','O']
   ]
 
   const checkWinCondition = () => {
-    // if player wins by columns
-    function winByRows(array) {
+    // if player wins by rows
+    function checkIfEqualRows(array) {
       if (array.every((element) => element === null)){
-        console.log(false)
-        return;
-      }
-
-      if (!array.every((element) => element === array[0])) {
-        console.log("not all equal")
-        return;
+        return false;
       }
 
       return array.every((element) => element === array[0])
     }
 
     if (
-      winByRows(gameBoard[0]) || 
-      winByRows(gameBoard[1]) || 
-      winByRows(gameBoard[2])
+      checkIfEqualRows(gameBoard[0]) || 
+      checkIfEqualRows(gameBoard[1]) || 
+      checkIfEqualRows(gameBoard[2])
     ) {
       console.log("you win by rows!")
     }
 
-    // if player wins by rows
-    function checkColumn(array, i, j, column) {
+    // if player wins by columns
+    function checkIfEqualColumns(array, i, j, column) {
       if (array[i][column] === null || array[j][column] === null || array[2][column] === null) {
         console.log("column isn't complete. check the other column")
         return;
@@ -66,15 +63,15 @@ const board = (() => {
     }
 
     if (
-      checkColumn(gameBoard, 0, 1, 0) || 
-      checkColumn(gameBoard, 0, 1, 1) || 
-      checkColumn(gameBoard, 0, 1, 2)
+      checkIfEqualColumns(gameBoard, 0, 1, 0) || 
+      checkIfEqualColumns(gameBoard, 0, 1, 1) || 
+      checkIfEqualColumns(gameBoard, 0, 1, 2)
     ) {
       console.log("you win by columns!")
     }
 
-    // if player wins diagonal 
-    function checkDiagonal(array) {
+    // if player wins by diagonal 
+    function checkIfEqualDiagonals(array) {
       if (array[1][1] === null) {
         return false;
       } 
@@ -90,16 +87,16 @@ const board = (() => {
       return false
     }
 
-    if (checkDiagonal(gameBoard)) {
+    if (checkIfEqualDiagonals(gameBoard)) {
       console.log("you win by diagonals")
     }
 
-    // if draw
+    // helper to check if board is full 
     function checkFull(array){
       if (array.every((element) => typeof element === 'string')){
         return true;
       } else {
-        false;
+        return false;
       }
     }
 
@@ -118,9 +115,18 @@ const board = (() => {
   }
 
   const displayBoard = () => {
+    const cells = document.querySelectorAll('.cell')
     for ( let row of gameBoard) {
       console.log(row);
     } 
+
+    for (let i = 0, j = 0, k = 0; i < 9; i++, k++) {
+      if (k > 2) {
+        j++
+        k = 0
+      }
+      cells[i].textContent = gameBoard[j][k]
+    }
   }
 
   const changeBoard = (row, column, change) => {
@@ -161,7 +167,7 @@ const players = {
 
 // 3. probably want an object to control the flow of the game.
 const gameFlow = {
-  
+
 }
 
 // Focus on getting a working game in the console first.
